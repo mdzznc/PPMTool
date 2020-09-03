c6671f0233b22c6a53b5c25342696ba086bc4ad4
mdzznc <mdzznc@gmail.com>
2020-09-02 21:22:41 -0400

    Backlog and ProjectTask entities established.

---
+++ PPMToolFullStack/src/main/java/io/agileintelligence/demo/repositories/BacklogRepository.java
@@ -0,0 +1,10 @@
+package io.agileintelligence.demo.repositories;
+
+import io.agileintelligence.demo.domain.Backlog;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog, Long> {
+
+}
