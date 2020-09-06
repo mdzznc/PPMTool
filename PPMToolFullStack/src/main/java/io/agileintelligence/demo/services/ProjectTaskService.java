05722900ae04d95c0021977454c21043dc5f4588
mdzznc <mdzznc@gmail.com>
2020-09-05 22:13:19 -0400

    Handles Project Not Found Exception.

--- PPMToolFullStack/src/main/java/io/agileintelligence/demo/services/ProjectTaskService.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/demo/services/ProjectTaskService.java
@@ -1,8 +1,11 @@
 package io.agileintelligence.demo.services;
 
 import io.agileintelligence.demo.domain.Backlog;
+import io.agileintelligence.demo.domain.Project;
 import io.agileintelligence.demo.domain.ProjectTask;
+import io.agileintelligence.demo.exceptions.ProjectNotFoundException;
 import io.agileintelligence.demo.repositories.BacklogRepository;
+import io.agileintelligence.demo.repositories.ProjectRepository;
 import io.agileintelligence.demo.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,29 +18,44 @@ public class ProjectTaskService {
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
 
+    @Autowired
+    private ProjectRepository projectRepository;
+
     public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask){
 
-        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
-        projectTask.setBacklog(backlog);
-        Integer BacklogSequence = backlog.getPTSequence();
-        BacklogSequence++;
-        backlog.setPTSequence(BacklogSequence);
-        projectTask.setProjectSequence(projectIdentifier+"-"+BacklogSequence);
-        projectTask.setProjectIdentifier(projectIdentifier);
+        try {
+            Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+            projectTask.setBacklog(backlog);
+            Integer BacklogSequence = backlog.getPTSequence();
+            BacklogSequence++;
+            backlog.setPTSequence(BacklogSequence);
+            projectTask.setProjectSequence(projectIdentifier+"-"+BacklogSequence);
+            projectTask.setProjectIdentifier(projectIdentifier);
 
-        if (projectTask.getStatus() == "" || projectTask.getStatus() == null){
-            projectTask.setStatus("TO_DO");
+            if (projectTask.getStatus() == "" || projectTask.getStatus() == null){
+                projectTask.setStatus("TO_DO");
+            }
+
+            if (projectTask.getPriority() == null){
+                projectTask.setPriority(3);
+            }
+
+            return projectTaskRepository.save(projectTask);
+
+        }catch (Exception e){
+            throw new ProjectNotFoundException("Project Not Found");
         }
-
-        if (projectTask.getPriority() == null){
-            projectTask.setPriority(3);
-        }
-
-        return projectTaskRepository.save(projectTask);
 
     }
 
     public Iterable<ProjectTask> findBacklogById(String id){
+
+        Project project = projectRepository.findByProjectIdentifier(id);
+
+        if (project == null){
+            throw new ProjectNotFoundException("Project with ID: ' "+id+"' does not exist");
+        }
+
         return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
     }
 
